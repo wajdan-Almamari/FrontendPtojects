d500e68e6b84274e719163ed9dcea7b5005e1800
wajdan <wejdan95.almamari@gmail.com>
2026-08-18 13:51:04 +0400

    2.2 Operators & Comparisons

--- Day4-JavaScript-Lab/warmups.js
+++ Day4-JavaScript-Lab/warmups.js
@@ -2,7 +2,20 @@
 const name = "Wajdan";
 
 let value = 0;
-console.log(typeof value ); //number
+console.log(typeof value); //number
 
 value = "Hello";
-console.log(typeof value ); //string
+console.log(typeof value); //string
+
+// 2.2 Operators & Comparisons
+
+console.log("10" == 10);   // true
+console.log("10" === 10);  // false
+
+const userName = "";
+
+if (userName) {
+    console.log("User name is entered");
+} else {
+    console.log("User name is empty");
+}
