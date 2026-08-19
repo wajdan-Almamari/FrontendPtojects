88ee459c6f5d128bf059fb649e38a206dc4bd8a7
wajdan <wejdan95.almamari@gmail.com>
2026-08-19 16:28:55 +0400

    improve code formatting and add new functions for array handling

--- Day5-Order-Analytics-Lab/warmups.js
+++ Day5-Order-Analytics-Lab/warmups.js
@@ -44,19 +44,42 @@ for (const city of cities) {
 //------------------------------------------------------
 //2.2	—	Destructuring
 console.log("-------------Destructuring--------------------");
-const order = { id : 101 , customer : "Sara Ahmed" , total : 249.99, status: "Shipped"};
-const {customer , total} = order;
+const order = { id: 101, customer: "Sara Ahmed", total: 249.99, status: "Shipped" };
+const { customer, total } = order;
 console.log(`customer : ${customer} , Totatl : ${total}`);
 
 // Array Destructuring + Rest
-const numbers = [13 , 0 , 8 , 95 ];
-const [first , second , ...rest] =numbers;
+const numbers = [13, 0, 8, 95];
+const [first, second, ...rest] = numbers;
 console.log(`first: ${first}`);
 console.log(`second: ${second}`);
 console.log(`rest: ${rest}`);
 
 // Destructuring directly in function parameter
-function orderSummery ({id , customer , total , status}){
+function orderSummery({ id, customer, total, status }) {
     return `${id} - ${customer} - ${total} - ${status}`;
 }
 console.log(orderSummery(order));
+
+//------------------------------------------------------
+//2.3	—	Spread	&	Rest
+console.log("-------------Spread & Rest--------------------");
+
+//Combine arrays using spread
+const onlineOrders = [101, 102, 103];
+const storeOrders = [201, 202, 203];
+
+const allOrders = [...onlineOrders, ...storeOrders];
+
+console.log(allOrders);
+
+const cancellOrder = { ...order , status : "CancelledOrder"};
+console.log("Original:", order);
+console.log("Copy:", cancellOrder);
+
+//Rest parameter
+function getTotals(...totals) {
+    return totals;
+}
+
+console.log(getTotals(100, 250, 75, 300));
