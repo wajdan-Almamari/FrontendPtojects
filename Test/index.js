abe1af2f7ac5a4e3a6364d73b1bfe979c3f5afb0
wajdan <wejdan95.almamari@gmail.com>
2026-08-20 15:03:21 +0400

    fix: add IDs to header and paragraph elements for JavaScript manipulation

---
+++ Test/index.js
@@ -0,0 +1,13 @@
+console.log(`Hello`);
+
+window.alert(`This is an alert`);
+
+document.getElementById("myH1").textContent =`hello`;
+document.getElementById("myP").textContent =`JavaScript`;
+
+
+let x= "";
+let y ="";
+let z = "";
+
+console.log(x, typeof x)
