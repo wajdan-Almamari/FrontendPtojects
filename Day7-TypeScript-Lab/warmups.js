56a6a5d5b452edca1a3d9557076d636f396bda9a
wajdan <wejdan95.almamari@gmail.com>
2026-09-15 09:47:44 +0400

    add: implement warmups TypeScript and JavaScript files for Day 7 lab

---
+++ Day7-TypeScript-Lab/warmups.js
@@ -0,0 +1,5 @@
+"use strict";
+let orderTotals = [100, 200, 300, 400, 500];
+let product = ["Laptop", 300];
+let customerName = "Wajdan";
+console.log(customerName);
--- Day7-TypeScript-Lab/warmups.ts
+++ Day7-TypeScript-Lab/warmups.ts
@@ -2,3 +2,7 @@ let orderTotals: number[] = [100, 200, 300, 400, 500];
 
 let product: [string, number] = ["Laptop", 300];
 let customerName = "Wajdan";
+
+let uncertainValue: unknown = "Hello TypeScript";
+
+console.log(customerName);
