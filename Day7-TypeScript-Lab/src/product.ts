8ee2c8c4689b05bd753e31640fea4ee49a78eb11
wajdan <wejdan95.almamari@gmail.com>
2026-09-15 11:42:42 +0400

    add: implement ProductEntity class extending Entity with properties and describe method

--- Day7-TypeScript-Lab/src/product.ts
+++ Day7-TypeScript-Lab/src/product.ts
@@ -10,3 +10,28 @@ export abstract class Entity {
 
     abstract describe(): string;
 }
+export class ProductEntity extends Entity implements Product {
+    name: string;
+    price: number;
+    stock: number;
+    category: string;
+
+    constructor(
+        id: number,
+        name: string,
+        price: number,
+        stock: number,
+        category: string
+    ) {
+        super(id);
+
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.category = category;
+    }
+
+    describe(): string {
+        return `${this.name} - ${this.price}`;
+    }
+}
